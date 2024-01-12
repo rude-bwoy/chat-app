842de40c4c3b2f66b77f7ad675dabd8ee0920380
Ivan <vidovic.ivan@outlook.com>
2024-01-12 14:44:31 +0100

    vraceno na tvornicke

--- src/Components/Messages.jsx
+++ src/Components/Messages.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import Avatar from "boring-avatars";
-// import ParticlesBg from 'particles-bg'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Avatar from "boring-avatars";
+import ParticlesBg from 'particles-bg'
 
 
 let drone = null;
@@ -79,11 +79,11 @@ export default function Messages() {
     }
 
 
-    // const toastify = () => {
-    //     toast.success(`New member joined!`, {
-    //         position: toast.POSITION.TOP_CENTER
-    //     });
-    // }
+    const toastify = () => {
+        toast.success(`New member joined!`, {
+            position: toast.POSITION.TOP_CENTER
+        });
+    }
 
     const sendMessage = (inputText) => {
         drone.publish({
@@ -109,7 +109,7 @@ export default function Messages() {
     return(
         <div className='main-container'>
               {/* <ToastContainer autoClose={1000}/> */}
-              {/* <ParticlesBg   
+              <ParticlesBg   
                             num={20} 
                             type="circle" 
                             bg={{
@@ -119,7 +119,7 @@ export default function Messages() {
                                 left: 0
                             }} 
                             color={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
-            /> */}
+            />
           <div className='main-flex-items right'>
             <div className='chat-container'>
                 <div className='messages'>
@@ -127,7 +127,7 @@ export default function Messages() {
                         {messages.map(message => 
                             <li key={message.messageId} data-messageid={message.messageId} className={message.clientId === myId ? "me" : ""}>
                                         <div className="message-content">
-                                            {/* <div className="avatar"><Avatar size={40} name={message.username} variant="beam" colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}/></div> */}
+                                            <div className="avatar"><Avatar size={40} name={message.username} variant="beam" colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}/></div>
                                             <div className={message.clientId === myId ? "outer messss" : "outer"}>
                                                 {message.clientId !== myId && 
                                                 <div className="username">{message.username}</div>} 
